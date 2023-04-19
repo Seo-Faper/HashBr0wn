a49d83e3a14cf1ee7316ec221924b80e2243b51e
seo-faper <dswhdcks@gmail.com>
2023-04-20 03:25:11 +0900

    chage message

--- hash/src/App.js
+++ hash/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div className='footer'>
         <div className='footer-message'>
           <center>
-            <p>Get Your HASH</p><br></br>
+            <p>Get Your HASH!</p><br></br>
             <p>by <a href="https://seo-faper.github.io/" target='noopener'>seo-faper</a></p>
           </center>
         </div>
